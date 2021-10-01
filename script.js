9a60940481571bdeeaf5b8626b4756c9bc0c5223
math-pixel <59537610+math-pixel@users.noreply.github.com>
2021-10-01 20:27:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ function showSlides() {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
+
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 } 
